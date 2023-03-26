b836f2b411ec7f09d568847fa7899536e1faf373
ROgbuagu05 <chizzy3003@gmail.com>
2023-03-26 06:49:34 -0700

    a

--- main.h
+++ main.h
@@ -39,3 +39,5 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
+
+#endif
