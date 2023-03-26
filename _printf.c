8ba9445835f0812508f73e36508aa18f13388f09
akinmuizz <akinmuizz@gmail.com>
2023-03-26 08:06:26 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -3,11 +3,10 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - Printf function to be used
  * @format: format.
  * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
